120fc8eb2d503331a2449021f23f48a288ef8993
Aaron Herres <a@octoblu.com>
2016-11-07 15:50:08 -0700

    v1.5.22 don't grab subscriptions for now

--- src/actions/MonitoredSubscriptionsGet/index.js
+++ src/actions/MonitoredSubscriptionsGet/index.js
@@ -10,10 +10,11 @@ export default function getMonitoredSubscriptions({uuid, meshbluConfig}) {
   const inquisitor = new Inquisitor({uuid, meshbluConfig})
   return dispatch => {
     dispatch(getMonitoredSubscriptionsRequest())
-    return inquisitor.getMonitoredDeviceSubscriptions((error, subscriptions) => {
-      if(error) return dispatch(getMonitoredSubscriptionsFailure(error))
-      _.each(subscriptions, (subscription) => dispatch(addEdge(subscription)))
-      return dispatch(getMonitoredSubscriptionsSuccess(subscriptions))
-    })
+    dispatch(getMonitoredSubscriptionsSuccess([]))
+    // return inquisitor.getMonitoredDeviceSubscriptions((error, subscriptions) => {
+    //   if(error) return dispatch(getMonitoredSubscriptionsFailure(error))
+    //   _.each(subscriptions, (subscription) => dispatch(addEdge(subscription)))
+    //   return dispatch(getMonitoredSubscriptionsSuccess(subscriptions))
+    // })
   }
 }
