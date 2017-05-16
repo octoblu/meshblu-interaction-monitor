f04899fe0e3f7ab59c91b79c5f07e428bd0f3696
redaphid / royvandewater <a@octoblu.com , r@octoblu.com>
2017-05-16 15:03:06 -0700

    v1.9.2 Animations!

--- src/components/InteractionSelectedMessage/index.js
+++ src/components/InteractionSelectedMessage/index.js
@@ -2,15 +2,16 @@ import React, { PropTypes } from 'react'
 import Button from 'zooid-button'
 import styles from './styles.css'
 
-const InteractionSelectedMessage = ({message, onSelectedMessagePanelHide, showSelectedMessagePanel}) => {
-  if (!showSelectedMessagePanel) return null
 
+const InteractionSelectedMessage = ({message, onSelectedMessagePanelHide, onSelectedMessagePanelShow, showSelectedMessagePanel}) => {
   const formattedMessage = _.isEmpty(message) ? null : JSON.stringify(message, null, 2)
+  const buttonClass = (showSelectedMessagePanel) ? styles.floatingButtonHide : styles.floatingButtonShow
+  const buttonOnClick = (showSelectedMessagePanel) ? onSelectedMessagePanelHide : onSelectedMessagePanelShow
 
   return (
     <div className={styles.root}>
-      <pre className={styles.selectedMessage}>{formattedMessage}</pre>
-      <Button kind="hollow-neutral" className={styles.floatingButton} onClick={onSelectedMessagePanelHide}>&lt;</Button>
+      <pre className={(showSelectedMessagePanel) ? styles.selectedMessage : styles.hidden}>{formattedMessage}</pre>
+      <Button kind="hollow-neutral" className={buttonClass} onClick={buttonOnClick} ><div>&lt;</div></Button>
     </div>
   )
 }
