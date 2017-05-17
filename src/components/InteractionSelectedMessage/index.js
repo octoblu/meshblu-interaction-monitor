d6c20db96bd7d70aea99f8e2fb1646874bee9b98
Roy van de Water <r@octoblu.com>
2017-05-17 10:05:20 -0700

    v1.9.4 Fix rotation direction of toggle button

--- src/components/InteractionSelectedMessage/index.js
+++ src/components/InteractionSelectedMessage/index.js
@@ -2,16 +2,30 @@ import React, { PropTypes } from 'react'
 import Button from 'zooid-button'
 import styles from './styles.css'
 
+const ToggleButton = ({message, onHide, onShow, showingPanel}) => {
+  if (!message) {
+    return <Button kind="hollow-neutral" className={styles.invisible} ><div>&lt;</div></Button>
+  }
+
+  if (showingPanel) {
+    return <Button kind="hollow-neutral" className={styles.floatingButtonHide} onClick={onHide} ><div>&lt;</div></Button>
+  }
+
+  return <Button kind="hollow-neutral" className={styles.floatingButtonShow} onClick={onShow} ><div>&lt;</div></Button>
+}
 
 const InteractionSelectedMessage = ({message, onSelectedMessagePanelHide, onSelectedMessagePanelShow, showSelectedMessagePanel}) => {
   const formattedMessage = _.isEmpty(message) ? null : JSON.stringify(message, null, 2)
-  const buttonClass = (showSelectedMessagePanel) ? styles.floatingButtonHide : styles.floatingButtonShow
-  const buttonOnClick = (showSelectedMessagePanel) ? onSelectedMessagePanelHide : onSelectedMessagePanelShow
 
   return (
     <div className={styles.root}>
       <pre className={(showSelectedMessagePanel) ? styles.selectedMessage : styles.hidden}>{formattedMessage}</pre>
-      <Button kind="hollow-neutral" className={buttonClass} onClick={buttonOnClick} ><div>&lt;</div></Button>
+      <ToggleButton
+        message={message}
+        onHide={onSelectedMessagePanelHide}
+        onShow={onSelectedMessagePanelShow}
+        showingPanel={showSelectedMessagePanel}
+        />
     </div>
   )
 }
